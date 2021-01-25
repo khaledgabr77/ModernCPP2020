9b6b91014df6762ba344e5fed6119c4baa788c9f
khaledgabr77 <khaledgabr77@gmail.com>
2021-01-26 01:11:25 +0200

    edit first task

---
+++ homework_1/help.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  int i = 0;
+  std::cout << "Hello SYS\n";
+  std::cout << "i is %d\n" << i << std::endl;
+  i++;
+  std::cout << "i is now %d\n" << i << std::endl;
+  return 0;
+}
--- homework_1/task_1/commands.sh
+++ homework_1/task_1/commands.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 #Prints the number of lines in a file
-wc -l ./data.dat
+#wc -l ./data.dat
+cat data.dat | wc -l
 
 #prints the number of line contian "", "" in a file
-grep -E "dolor|dalor" ./data.dat
+#grep -E "dolor|dalor" ./data.dat
+((grep dolor data.dat) || (grep dalor data.dat)) | wc -l
 
 #prints the number of words in a file
-wc -w ./data.dat
-
+#wc -w ./data.dat
+cat data.dat | wc -w
 
 #print the number of word starting with "mol" in file
-grep -c mol ./data.dat
-
+#grep -c mol ./data.dat
+cat -n data.dat | grep mol | wc -w 
 
