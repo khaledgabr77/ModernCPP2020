833e4a2382a90ffd6bafcd443694628e66dbd641
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-06 01:58:55 +0200

    lec4 ex

---
+++ homework_4/working/Vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+  std::vector<int> num = {1, 2, 3};
+  std::vector<std::string> names = {"khaled", "mahomud}"};
+
+  names.emplace_back("Gabr");
+
+  std::cout << "first name: " << names.front() << std::endl;
+  std::cout << "last num: " << num.back() << std::endl;
+  std::cout << "first name: " << names.back() << std::endl;
+}
