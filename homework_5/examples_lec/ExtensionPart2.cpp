84c2e5345721a6ef54708258c699131e9089a1c7
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-12 18:13:03 +0200

    extension part2

---
+++ homework_5/examples_lec/ExtensionPart2.cpp
@@ -0,0 +1,9 @@
+#include <filesystem>
+#include <iostream>
+namespace fs = std ::filesystem;
+int main() {
+  std::cout << fs::path("/foo /.").extension() << '\n'
+            << fs::path("/foo /..").extension() << '\n'
+            << fs::path("/foo /. hidden ").extension() << '\n'
+            << fs::path("/foo /.. bar").extension() << '\n';
+}
