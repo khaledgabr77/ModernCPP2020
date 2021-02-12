49b59d9d65aa8a18f6c1f1a041a8f8003c2292f6
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-12 18:10:51 +0200

    FileExtension

---
+++ homework_5/examples_lec/Extension.cpp
@@ -0,0 +1,11 @@
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+int main() {
+  std::cout << fs::path("/foo/bar.txt").extension() << "\n"
+            << fs::path("/foo/bar.").extension() << "\n"
+            << fs::path("/foo/bar").extension() << "\n"
+            << fs::path("/foo/bar.png").extension() << "\n";
+}
