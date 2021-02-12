2bcd3b5efbf9e2f5cee997484b18e2c8d47deffa
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-12 18:02:21 +0200

    File_Part2

---
+++ homework_5/examples_lec/File_Part2.cpp
@@ -0,0 +1,12 @@
+#include <filesystem>
+#include <iostream>
+
+namespace fs = std::filesystem;
+
+int main() {
+  std::cout << fs::path("/foo/.bar").filename() << '\n'
+            << fs::path(".").filename() << "\n"
+            << fs::path("..").filename() << "\n"
+            << fs::path("/").filename() << "\n"
+            << fs::path("//host") << "\n";
+}
