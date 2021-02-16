4e9a8b242185ed87a57a23fb466ab4ceb0639252
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-17 00:06:39 +0200

    Blank Image

---
+++ OpenCv/build/CMakeFiles/blank.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blank.dir/src/BlankImage.cpp.o"
+  "blank.pdb"
+  "blank"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
