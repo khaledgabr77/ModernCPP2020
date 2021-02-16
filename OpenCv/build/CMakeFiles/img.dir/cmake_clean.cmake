41882ba261852c4923857f7e7c43d667641f0115
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-16 23:55:15 +0200

    Load and Display images

---
+++ OpenCv/build/CMakeFiles/img.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/img.dir/src/Load&Dispaly.cpp.o"
+  "img.pdb"
+  "img"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/img.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
