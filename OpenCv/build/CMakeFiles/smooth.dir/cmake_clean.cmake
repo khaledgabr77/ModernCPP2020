d93b4226f2a762b3be3b947d1cdbdce0a1397ac2
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-21 23:46:37 +0200

    using gaussin and median BLUR

---
+++ OpenCv/build/CMakeFiles/smooth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smooth.dir/src/smoothing.cpp.o"
+  "smooth.pdb"
+  "smooth"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/smooth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
