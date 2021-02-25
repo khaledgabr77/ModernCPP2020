0c543c0da8a3795fb1144ce9acf50c01cc675458
khaledgabr77 <khaledgabr77@gmail.com>
2021-02-25 12:08:00 +0200

    Hist Comparision

---
+++ OpenCv/build/CMakeFiles/comp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/comp.dir/src/HistComparison.cpp.o"
+  "comp.pdb"
+  "comp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/comp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
